430ccb199508e3222babb57cef4ef1db28c288f9
Wout Elstgeest <w.elstgeest@inepro.com>
2020-02-21 23:05:31 +0100

    Fix tests with strict check

--- test.ts
+++ test.ts
@@ -1,4 +1,3 @@
-import { test, runIfMain } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { sha256 } from "./mod.ts";
 
@@ -11,38 +10,36 @@ interface TestVector {
 /** Deserializes a Uint8Array from a hexadecimal string. */
 function hex2buf(hex: string): Uint8Array {
   const len: number = hex.length;
-  
+
   if (len % 2 || !/^[0-9a-fA-F]*$/.test(hex)) {
     throw new TypeError("Invalid hex string.");
   }
-  
+
   hex = hex.toLowerCase();
-  
+
   const buf: Uint8Array = new Uint8Array(Math.floor(len / 2));
   const end: number = len / 2;
-  
+
   for (let i: number = 0; i < end; ++i) {
     buf[i] = parseInt(hex.substr(i * 2, 2), 16);
   }
-  
+
   return buf;
 }
 
-const testVectors: TestVector[] = JSON.parse(
+const testVectors: TestVector[] = (JSON.parse(
   new TextDecoder().decode(Deno.readFileSync("./test_vectors.json"))
-).map(({ msg, msg_bit_len, hash }): TestVector => ({
+) as any[]).map(({ msg, msg_bit_len, hash }): TestVector => ({
     msg: hex2buf(msg),
     msg_bit_len,
     hash: hex2buf(hash)
   }));
 
 testVectors.forEach(({ msg, msg_bit_len, hash }: TestVector) => {
-  test({
+  Deno.test({
     name: `SHA2-256 ${msg_bit_len ? msg_bit_len / 8 : 0}-byte msg`,
     fn(): void {
       assertEquals(sha256(msg), hash);
     }
   });
 });
-
-runIfMain(import.meta, { parallel: true });
